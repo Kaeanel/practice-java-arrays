171ccb9db6a9ec34f4a472fba812d4808ac9cae6
Denis <denisfilipov.12@gmail.com>
2019-11-05 23:46:00 +0100

    Exercise complete!

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,22 @@ public class Main {
         Console console = System.console();
 
         // TODO: 1. Create an array of good cow names (this should be a String array).
-
+        String[] cowNames = {"Mike","Will","Bernie"};
 
         // TODO: 2. Create an array of Cows the same length as your 'names' array.
-
+        Cow[] cows = new Cow[cowNames.length];
 
         // TODO: 3. Use a for loop to populate the 'cows' array with Cows using names from the 'names' array.
-
+        for (int i = 0; i < cows.length; i++) {
+            cows[i] = new Cow(cowNames[i]);
+        }
 
         // TODO: 4. Use an enhanced for loop to print the name of each Cow.
+        for (Cow cow : cows) {
+            System.out.printf("Hello, my name is %s%n",
+                              cow.getName());
+
+        }
 
     }
 }
